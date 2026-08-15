67e50f1a960c7e45d528c45a34e49cf2d07a2214
mcbsq <miguelcq13@gmail.com>
2026-08-14 19:14:20 -0600

    chore: scaffold Next.js 15 project with TypeScript and Tailwind

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import nextPlugin from "eslint-config-next";
+
+const eslintConfig = [
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "node_modules/**",
+    ],
+  },
+  nextPlugin,
+];
+
+export default eslintConfig;
